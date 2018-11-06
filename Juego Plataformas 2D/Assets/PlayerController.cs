e058e3694f4a7f847f7598bf48073ad049c39c2f
CesarDiazDelgado <odioelingles17@gmail.com>
2018-11-06 12:47:29 +0100

    Escalado personaje y seguimiento de cámara

--- Juego Plataformas 2D/Assets/PlayerController.cs
+++ Juego Plataformas 2D/Assets/PlayerController.cs
@@ -60,12 +60,12 @@ public class PlayerController : MonoBehaviour
 
         if (h > 0.1f)                                               //Cambiar la orientación del personaje
         {
-            transform.localScale = new Vector3(-0.2f, 0.2f, 0.2f);
+            transform.localScale = new Vector3(-0.49f, 0.49f, 0.49f);
         }
 
         if (h < -0.1f)
         {
-            transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            transform.localScale = new Vector3(0.49f, 0.49f, 0.49f);
         }
 
         if (jump)
